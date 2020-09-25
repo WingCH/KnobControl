fdeddf2b896bc78d0eca24b95e7f9488360d22de
WingCH <wingchan@vgt-hk.net>
2020-09-25 15:10:55 +0800

    add Carthage Example

---
+++ Example/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by WingCH on 25/9/2020.
+//
+
+import UIKit
+import KnobControl
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var knobView: Knob!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
